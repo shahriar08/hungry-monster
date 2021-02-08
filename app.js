40b91425d0c982db4a8fce1e347fe74ed3f084e3
shahriar08 <anik7744@gmail.com>
2021-02-08 23:32:14 +0600

    Complete Search Card Meal Item

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const getMeal = () =>{ 
     let search = document.getElementById('search').value;
     let mainNode = document.getElementById('showItems');
@@ -8,11 +10,12 @@ const getMeal = () =>{
                 let mealItemsDiv = document.createElement('div');
                 mealItemsDiv.className = 'col-md-3 item-columns';
                 let cardHtml = `
+                <div onclick="getMealElement(${meal.idMeal})"  class="card rounded-3 border-0">
                 <img src = "${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title food-title text-center">${meal.strMeal}</h5>
-                </div>
-                `;
+                    <h5 class="text-center">${meal.strMeal}</h5>
+                </div>`;
+
                 mealItemsDiv.innerHTML = cardHtml;
                 mainNode.appendChild(mealItemsDiv);
             });
@@ -25,3 +28,65 @@ document.getElementById('searchButton').addEventListener('click', function(){
     getMeal();
 
 })
+
+
+         // Show Meals Ingredient
+
+const getMealElement = meals => {
+    document.getElementById('show-ingredient').innerText = "";
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meals}`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
+        const mealItem = data.meals[0] ;
+        const keys = Object.keys(mealItem);
+
+        const itemsArray = [];
+        const measureArray = [];
+        keys.forEach(key => {
+            if (key.startsWith('strIngredient') && mealItem[key] != "" && mealItem[key] != null ) {
+                itemsArray.push(mealItem[key])
+            }
+        });
+        
+        keys.forEach(key => { 
+            if (key.startsWith('strMeasure') && mealItem[key] != "" && mealItem[key] != null && mealItem[key] != " " ) {
+                measureArray.push(mealItem[key])
+            }
+        });
+
+        const showIngredients = document.getElementById('show-ingredient');
+        const ingredientItem = document.createElement('div')
+
+        const card = `
+        <div class="row d-flex justify-content-centerd-flex justify-content-center">
+              <div class="col-md-6 ingDiv">
+                  <div class="ing-image">
+                    <img src="${meal.strMealThumb}" width="100%" alt="">
+                  </div>
+                  <h2>${meal.strMeal}</h2><br>
+                  <h5 >Ingredients</h5><br>
+                  <div id="ingredient">
+                  </div> 
+              </div>
+        </div>
+        `
+        ingredientItem.innerHTML = card;
+        showIngredients.appendChild(ingredientItem);
+
+        const ingredientList = document.getElementById('ingredient');
+        let allItems = ``;
+
+        itemsArray.forEach((ingredient , index) => {
+            const measure = measureArray[index];
+            allItems += `
+            <li> ${measure} ${ingredient}</li>
+            `     
+        });
+        ingredientList.innerHTML = allItems;   
+
+        
+    })
+}
+
+
